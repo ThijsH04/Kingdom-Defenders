f0837e0097c1bd8eede16f07b62f039e3007cdad
ThijsH04 <ThijsH04@gmail.com>
2023-02-05 23:34:52 +0100

    start of parent class Tower

---
+++ src/js/tower.js
@@ -0,0 +1,25 @@
+class Tower{
+    constructor(id,x,y,img,cost){
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.img = img;
+        this.cost = cost;
+    }
+
+    update(ctx, tileSize){
+        // to be implemented
+        this.render();
+    }
+
+    render(ctx, tileSize){
+        // to be implemented
+        
+    }
+
+    sell(){
+        // to be implemented        
+    }
+
+
+}
