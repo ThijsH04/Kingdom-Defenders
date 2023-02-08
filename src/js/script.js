37d3e62c88f89f264365f93e55125b057f98b7d3
Lukas Potters <lukaspotters@gmail.com>
2023-02-08 22:08:31 +0100

    game upgrade system

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,8 @@ window.onload = () => {
     game.world = new WorldMap(game, 0, "World 1")
     game.map = new GameMap(0)
     game.tutorial = new Tutorial(game)
-    game.updateCanvasSize()
+    game.upgrades = new Upgrades(game, upgrades)
+    game.setMode("map", false)
     function update() {
         game.update()
         requestAnimationFrame(update)
