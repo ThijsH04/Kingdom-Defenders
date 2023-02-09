d1c5302946e01fda229d2e5a8113839b97a3a4ec
Lukas Potters <lukaspotters@gmail.com>
2023-02-09 10:25:16 +0100

    multiple upgrade trees

--- src/js/upgrades.js
+++ src/js/upgrades.js
@@ -2,7 +2,7 @@ class Upgrades {
     constructor(game, trees) {
         this.upgradeTrees = []
 
-        this.showBtn = document.createElement("div")
+        this.showBtn = document.createElement("button")
         this.showBtn.className = "upgrade-btn"
         this.showBtn.onclick = () => {
             game.setMode(game.mode == "upgrade" ? "map" : "upgrade")
@@ -26,6 +26,7 @@ class Upgrades {
         for(let t=0;t<trees.length;t++) {
             this.upgradeTrees.push(new UpgradeTree(trees[t], this.element, this))
         }
+        this.upgradeTrees[0].show(this)
     }
     selectNode(node) {
         console.log(node)
@@ -47,6 +48,7 @@ class Upgrades {
         let closeBtn = document.createElement("button")
         closeBtn.className = "base-btn"
         closeBtn.innerHTML = "close"
+
         closeBtn.onclick = () => {
             this.selectNode(false)
         }
@@ -54,14 +56,27 @@ class Upgrades {
 
         let buyBtn = document.createElement("button")
         buyBtn.className = "base-btn"
-        buyBtn.innerHTML = "buy"
         buyBtn.onclick = () => {
             this.selectNode(false)
             node.unlock()
         }
+        if(node.unlocked) {
+            buyBtn.disabled = true
+            buyBtn.innerHTML = "Owned"
+        } else if(!node.unlockable) {
+            buyBtn.disabled = true
+            buyBtn.innerHTML = "Locked"
+        } else {
+            buyBtn.innerHTML = "Buy"
+        }
         this.upgradeInfo.appendChild(buyBtn)
 
-
+    }
+    hideTrees() {
+        for(let t=0;t<this.upgradeTrees.length;t++) {
+            this.upgradeTrees[t].box.style.display = "none"
+            this.upgradeTrees[t].selectBtn.style.background = "#0005"
+        }
     }
 }
 
@@ -101,6 +116,20 @@ class UpgradeTree {
         for(let n=0;n<tree.nodes.length;n++) {
             this.nodes.push(new UpgradeNode(tree.nodes[n], elem, upgradeObj))
         }
+
+
+        this.selectBtn = document.createElement("button")
+        this.selectBtn.className = "select-btn"
+        this.selectBtn.innerHTML = this.type
+        this.selectBtn.onclick = () => {
+            this.show(upgradeObj)
+        }
+        upgradeObj.infoBar.appendChild(this.selectBtn)
+    }
+    show(upgradeObj) {
+        upgradeObj.hideTrees()
+        this.selectBtn.style.background = "#aaa5"
+        this.box.style.display = "block"
     }
 }
 
@@ -116,7 +145,7 @@ class UpgradeNode {
         this.children = []
 
         this.element = document.createElement("li")
-        this.element.div = document.createElement("div")
+        this.element.div = document.createElement("button")
 
         this.element.div.className = "upgrade-node"
         this.element.div.innerHTML = node.name
@@ -151,89 +180,117 @@ class UpgradeNode {
 
 upgrades = [
     {
-    type: "normal",
-    nodes: [
-        {
-            name: "base upgrade",
-            description: "very cool and awesome upgrade",
-            cost: 0,
-            unlocks: [],
-            nodes: [
-                {
-                    name: "sub upgrade",
-                    description: "hey look this upgrade can be developed further",
-                    cost: 1,
-                    unlocks: [],
-                    nodes: [
-                        {
-                            name: "sub sub upgrade",
-                            description: "it goes deeper",
-                            cost: 1,
-                            unlocks: [],
-                            nodes: [
-                                {
-                                    name: "test upgrade",
-                                    description: "",
-                                    cost: 1,
-                                    unlocks: [],
-                                    nodes: []
-                                },
-                                {
-                                    name: "test upgrade",
-                                    description: "",
-                                    cost: 1,
-                                    unlocks: [],
-                                    nodes: []
-                                },
-                                {
-                                    name: "test upgrade",
-                                    description: "",
-                                    cost: 1,
-                                    unlocks: [],
-                                    nodes: []
-                                },
-                                {
-                                    name: "test upgrade",
-                                    description: "",
-                                    cost: 1,
-                                    unlocks: [],
-                                    nodes: []
-                                },
-                                {
-                                    name: "test upgrade",
-                                    description: "",
-                                    cost: 1,
-                                    unlocks: [],
-                                    nodes: []
-                                },
-                                {
-                                    name: "test upgrade",
-                                    description: "",
-                                    cost: 1,
-                                    unlocks: [],
-                                    nodes: []
-                                },
-                                
-                            ]
-                        },
-                        {
-                            name: "sub sub upgrade",
-                            description: "it goes deeper",
-                            cost: 1,
-                            unlocks: [],
-                            nodes: []
-                        }
-                    ]
-                },
-                {
-                    name: "sub upgrade",
-                    description: "hey look this upgrade can be developed further",
-                    cost: 1,
-                    unlocks: [],
-                    nodes: []
-                }
-            ]
-        }
-    ]
-}
+        type: "normal",
+        nodes: [
+            {
+                name: "base upgrade",
+                description: "very cool and awesome upgrade",
+                cost: 0,
+                unlocks: [],
+                nodes: [
+                    {
+                        name: "sub upgrade",
+                        description: "hey look this upgrade can be developed further",
+                        cost: 1,
+                        unlocks: [],
+                        nodes: [
+                            {
+                                name: "sub sub upgrade",
+                                description: "it goes deeper",
+                                cost: 1,
+                                unlocks: [],
+                                nodes: [
+                                    {
+                                        name: "sub sub sub upgrade",
+                                        description: "",
+                                        cost: 1,
+                                        unlocks: [],
+                                        nodes: []
+                                    },
+                                    {
+                                        name: "sub sub sub upgrade",
+                                        description: "",
+                                        cost: 1,
+                                        unlocks: [],
+                                        nodes: []
+                                    },
+                                    {
+                                        name: "sub sub sub upgrade",
+                                        description: "",
+                                        cost: 1,
+                                        unlocks: [],
+                                        nodes: []
+                                    },
+                                    {
+                                        name: "sub sub sub upgrade",
+                                        description: "",
+                                        cost: 1,
+                                        unlocks: [],
+                                        nodes: []
+                                    },
+                                    {
+                                        name: "sub sub sub upgrade",
+                                        description: "",
+                                        cost: 1,
+                                        unlocks: [],
+                                        nodes: []
+                                    },
+                                    {
+                                        name: "sub sub sub upgrade",
+                                        description: "",
+                                        cost: 1,
+                                        unlocks: [],
+                                        nodes: []
+                                    },
+                                    
+                                ]
+                            },
+                            {
+                                name: "sub sub upgrade",
+                                description: "it goes deeper",
+                                cost: 1,
+                                unlocks: [],
+                                nodes: []
+                            }
+                        ]
+                    },
+                    {
+                        name: "sub upgrade",
+                        description: "hey look this upgrade can be developed further",
+                        cost: 1,
+                        unlocks: [],
+                        nodes: []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        type: "test",
+        nodes: [
+            {
+                name: "base upgrade",
+                description: "very cool and awesome upgrade",
+                cost: 0,
+                unlocks: [],
+                nodes: [
+                    {
+                        name: "sub upgrade",
+                        description: "hey look this upgrade can be developed further",
+                        cost: 1,
+                        unlocks: [],
+                        nodes: [
+                            {
+                                name: "cool upgrade",
+                                description: "brrr",
+                                cost: 1,
+                                unlocks: [],
+                                nodes: []
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
 ]
